11fe9a131a29b9ddfec920ac6af5c53450a74ade
wgyYOYO <2543054794@qq.com>
2021-11-06 00:19:07 +0800

    引入element

---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button>你好</button>
+    <el-button>这是element</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
